2b89faba1dd56ed8b7ba5de3734ab939755132a5
karlinhaa <148370537+karlinhaa@users.noreply.github.com>
2024-09-23 10:56:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+criaCartao(
+    'Programação',
+    'o que é Python?',
+    'O python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da França é Paris'
+)
